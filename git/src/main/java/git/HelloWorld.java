8ec7aed83706ada7bb53f719a3b3536cf899bc16
bhavanac1 <BChigurupati@aceinfosolutions.com>
2021-05-26 22:29:58 -0400

    Next Commit

--- git/src/main/java/git/HelloWorld.java
+++ git/src/main/java/git/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test Hello World");
+		System.out.println("New Test Hello World");
 
 	}
 
